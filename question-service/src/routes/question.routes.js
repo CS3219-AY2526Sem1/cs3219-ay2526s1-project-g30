05d0ca653db980a02f726d65a54b070fa9a75002
changjy81 <changjy81@gmail.com>
2025-10-24 17:57:06 +0800

    feat: add fetching, editing or deleting specific questions

--- question-service/src/routes/question.routes.js
+++ question-service/src/routes/question.routes.js
@@ -3,7 +3,7 @@ import Question from '../models/question.model.js';
 
 const router = express.Router();
 
-// Get a random question by difficulty and category
+// get a random question by difficulty and category
 router.get('/randomQuestion', async (req, res) => {
     try {
         const { difficulty, category } = req.query;
@@ -30,7 +30,19 @@ router.get('/randomQuestion', async (req, res) => {
     }
 });
 
+// get a specific question by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const question = await Question.findById(id);
+        if (!question) return res.status(404).json({ message: 'Question not found.' });
+        res.json(question);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
+// add a single question
 router.post('/', async (req, res) => {
     try {
         const {title, description, difficulty, category, examples, function_name, function_params} = req.body;
@@ -58,5 +70,32 @@ router.post('/', async (req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+// edit a single question by ID
+router.post('/edit/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+
+        const updatedQuestion = await Question.findByIdAndUpdate(id, updates, { new: true });
+        if (!updatedQuestion) return res.status(404).json({ message: 'Question not found.' });
+
+        res.json(updatedQuestion);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// remove a single question by ID
+router.post('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedQuestion = await Question.findByIdAndDelete(id);
+        if (!deletedQuestion) return res.status(404).json({ message: 'Question not found.' });
+        res.status(200).json(deletedQuestion);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 
 export default router;
